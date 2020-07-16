455ff01cbb20964bc9bb042526675c0686d2cd04
Bao <lbx01072004@gmail.com>
2020-07-16 22:26:23 +1000

    Updates

---
+++ precache-manifest.7ee2153fbe1dd202957dc8193edd1a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df49befb4e8c431df386",
+    "url": "/static/css/main.5394f678.chunk.css"
+  },
+  {
+    "revision": "df49befb4e8c431df386",
+    "url": "/static/js/main.12dda94f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16afe850ba36d8a4b5a7",
+    "url": "/static/js/2.43bcd1cc.chunk.js"
+  },
+  {
+    "revision": "6548b81ea4d9d9c77f60a52089b781f9",
+    "url": "/index.html"
+  }
+];
